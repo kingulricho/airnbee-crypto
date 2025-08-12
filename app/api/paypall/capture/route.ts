1c6f4c19badf9d17e9f9b988e1ca34a15f169df9
tsadjioulrich@gmail.com <tsadjioulrich@gmail.com>
2025-08-12 06:26:42 -1100

    paypal

--- app/api/paypall/capture/route.ts
+++ app/api/paypall/capture/route.ts
@@ -20,7 +20,7 @@ const client = new Client({
         oAuthClientSecret: PAYPAL_CLIENT_SECRET!,
     },
     timeout: 0,
-    environment: Environment.Sandbox,
+    environment: Environment.Production,
     logging: {
         logLevel: LogLevel.Info,
         logRequest: { logBody: true },
